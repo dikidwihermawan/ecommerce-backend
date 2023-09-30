e4ff85ab7b4282665b2220078d15c012b10b4fd1
Diki Dwi Hermawan <dikidwihermawan@gmail.com>
2023-09-30 15:03:12 +0700

    fixed bug

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
+use App\Http\Controllers\Auth\{
+    LoginController,
+    LogoutController,
+    MeController,
+    RegisterController
+};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::post('/register', RegisterController::class);
+Route::post('/login', LoginController::class);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('me', MeController::class);
+    Route::post('logout', LogoutController::class);
 });
